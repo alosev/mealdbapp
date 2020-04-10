72f9f45e17e2ccb359ac60ca39acb484816531a7
Alexey <lexa2201@mail.ru>
2020-04-10 10:56:13 +0300

    Init

---
+++ app/src/main/java/ru/teledoc/studio/mealdb/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.teledoc.studio.mealdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
